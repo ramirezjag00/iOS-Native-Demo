3e8a160b3eb16db502ea91cede4abfd63a724e54
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-06-18 00:35:08 +0800

    Add the struct & self, coding exercise

---
+++ CodingExercises/CodingExercise10.swift
@@ -0,0 +1,40 @@
+func exercise() {
+
+    // Define the User struct here
+    struct User {
+        var name: String
+        var email: String?
+        var followers: Int
+        var isActive: Bool
+        
+        init(name: String, email: String?, followers: Int, isActive: Bool) {
+            self.name = name
+            self.email = email
+            self.followers = followers
+            self.isActive = isActive
+        }
+        
+        func logStatus() {
+            if (isActive) {
+                print("\(name) is working hard")
+            } else {
+                print("\(name) has left earth")
+            }
+        }
+    }
+
+    // Initialise a User struct here
+    var richard = User(name: "Richard", email: nil, followers: 0, isActive: false)
+    richard.logStatus()
+
+    // Diagnostic code - do not change this code
+    print("\nDiagnostic code (i.e., Challenge Hint):")
+    var musk = User(name: "Elon", email: "elon@tesla.com", followers: 2001, isActive: true)
+    musk.logStatus()
+    print("Contacting \(musk.name) on \(musk.email!) ...")
+    print("\(musk.name) has \(musk.followers) followers")
+    // sometime later
+    musk.isActive = false
+    musk.logStatus()
+    
+}
