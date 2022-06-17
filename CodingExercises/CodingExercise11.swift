508b8f385e31beb0603b12e728e485e54e9e471e
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-06-18 03:29:43 +0800

    Add function with return coding exercise

---
+++ CodingExercises/CodingExercise11.swift
@@ -0,0 +1,11 @@
+//Create your function here:
+
+// isOdd function which receives inner/outer n name Int parameter which returns Bool true if n is Odd otherwise false
+func isOdd(n: Int) -> Bool {
+    if n % 2 != 0 {
+        return true 
+    } else {
+        return false
+    }
+}
+
