bd8d6fb189903c8f59c1bde0985c832889848bc3
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-07-03 11:37:27 +0800

    Update loop coding exercise

--- CodingExercises/CodingExercise12.swift
+++ CodingExercises/CodingExercise12.swift
@@ -2,17 +2,19 @@ class Assignment {
     
     // print fibonacci sequence with length n
     func fibonacci(n: Int) {
-        var fibSeq = [0, 1]
-        var i = fibSeq.count
+  	var fibSeq = [0, 1]
+  	var i = fibSeq.count
 
-        while n > i {
-            fibSeq.append(fibSeq[i - 2] + fibSeq[i - 1])
-            i+=1
-        }
-        
-        print(fibSeq)
-    
-    }
-    
-    
+  	if (n == 0) {
+  	  print("Invalid")
+  	} else if (n == 1) {
+   	  print([0])
+  	} else {
+    	  while n > i {
+      	    fibSeq.append(fibSeq[i - 2] + fibSeq[i - 1])
+      	    i+=1
+    	  }
+    	  print(fibSeq)
+  	}
+    }		
 }
