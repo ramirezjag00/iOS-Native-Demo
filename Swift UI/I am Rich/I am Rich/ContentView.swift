dff2892e423778eb5e45a5f49ce41a149e6edff5
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-07-06 11:18:09 +0800

    Add changes to i am rich - swift ui project

--- Swift UI/I am Rich/I am Rich/ContentView.swift
+++ Swift UI/I am Rich/I am Rich/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+        
     }
 }
 
